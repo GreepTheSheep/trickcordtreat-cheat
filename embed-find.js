6800d238d037c3ca415cd53356dd13dd2d7394ee
Greep <89matt89.md@gmail.com>
2020-10-22 14:33:37 +0200

    Update embed-find.js

--- embed-find.js
+++ embed-find.js
@@ -1,34 +1,26 @@
-function findInMessage(message, target, {
-    caseSensitive = false,
+function findInMessage(message, target){
+let params = {
     author = false,
     description = true,
     footer = true,
     title = true,
     fields = true
-  }) {
+  }
     if (!target || !message) return null;
-    let str = caseSensitive ? target : target.toLowerCase();
+    let str = target
   
-    if ((caseSensitive && message.content.includes(str)) ||
-      (!caseSensitive && message.content.toLowerCase().includes(str))) return true;
+    if (message.content.toLowerCase().includes(str)) return true;
   
     for (let embed of message.embeds) {
-      if ((caseSensitive && (
-          (author && embed.author.includes(str)) ||
-          (description && embed.description.includes(str)) ||
-          (footer && embed.footer.includes(str)) ||
-          (title && embed.title.includes(str)))) ||
-        (!caseSensitive && (
-          (author && embed.author.toLowerCase().includes(str)) ||
-          (description && embed.description.toLowerCase().includes(str)) ||
-          (footer && embed.footer.toLowerCase().includes(str)) ||
-          (title && embed.title.toLowerCase().includes(str))))
+      if ((params.author && embed.author.toLowerCase().includes(str)) ||
+          (params.description && embed.description.toLowerCase().includes(str)) ||
+          (params.footer && embed.footer.toLowerCase().includes(str)) ||
+          (params.title && embed.title.toLowerCase().includes(str))
       ) return true;
   
-      if (fields)
+      if (params.fields)
         for (let field of embed.fields) {
-          if ((caseSensitive && [field.name, field.value].includes(str)) ||
-            (!caseSensitive && [field.name.toLowerCase(), field.value.toLowerCase()].includes(str))) return true;
+          if ([field.name.toLowerCase(), field.value.toLowerCase()].includes(str)) return true;
         }
     }
   
