9e200d0296ba1e4becd5b3475aa984b5abf5b30a
Greep <89matt89.md@gmail.com>
2020-10-22 20:19:39 +0200

    Add delay

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const {token, selectedGuilds} = require('./config.json')
+const {token, selectedGuilds, delay} = require('./config.json')
 const findInMessage = require('./embed-find.js')
+const wait = require('util').promisify(setTimeout);
 client.login(token)
+
+if (!delay) var delay = 600
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}\nTrick'cord Treat, Let's fight!`)
-    if (selectedGuilds == "any"){
+    if (selectedGuilds == "any" || !selectedGuilds){
         var selectedGuilds = []
         client.guilds.forEach(g=>{
             selectedGuilds.push(g.id)
@@ -14,14 +17,16 @@ client.on('ready', () => {
     }
 })
 
-client.on('message', message => {
+client.on('message', async message => {
     if (message.author.id == '755580145078632508'){
         if (selectedGuilds.includes(message.guild.id)){
             if (findInMessage(message, 'h!trick')){
                 console.log('h!trick found on server: ' + message.guild.name)
+                await wait(delay)
                 message.channel.send('h!trick')
             } else if (findInMessage(message, 'h!treat')){
                 console.log('h!treat found in server: ' + message.guild.name)
+                await wait(delay)
                 message.channel.send('h!treat')
             }
         }  
